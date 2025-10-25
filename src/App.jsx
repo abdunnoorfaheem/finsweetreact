f44880e9ba7227ac630e94dd9b7d0e0a4c4dde0d
eaheabinayub <eahea.sbmc403@gmail.com>
2025-10-25 19:34:18 +0600

    Pricing part completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import RootLayout from './components/layouts/RootLayout'
-import Home from './components/pages/Home'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import RootLayout from "./components/layouts/RootLayout";
+import Home from "./components/pages/Home";
+import Pricing from "./components/pages/Pricing";
 
 function App() {
-
-
   return (
     <>
-    <Routes>
-      <Route path='/' element={<RootLayout/>}>
-      <Route index element={<Home/>}/>
-      </Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
