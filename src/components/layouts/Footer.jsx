4667accf72cc978cfbe68623f4ad59824ac977ad
abdunnoorfaheem <abdunnoor3030@gmail.com>
2025-10-27 01:28:49 +0600

    header and footer update

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -9,68 +9,87 @@ import { IoLogoLinkedin } from "react-icons/io";
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
-   
-    return (
-        <>
-            <footer>
-                {/* footer top start */}
-                <section className='bg-quaternary pt-[100px] pb-7 text-white font-poppins '>
-                    <Container>
-                        <div className={"md:flex pb-24 text-center md:text-left relative"}>
-                            <div className="w-[49%] m-auto ">
-                                <Image imgSrc={Logo} />
-                                <Paragraph className={"w-[359px] pt-[22px] leading-7 "}>We are always open to discuss your project and improve your online presence.</Paragraph>
-                                <div className="bg-quinary w-[500px] md:max-w-[517px] h-[95px]  absolute md:-bottom-7 bottom-0 left-0">
-                                    <Flex className={"gap-x-12 text-black py-[17px] pl-[37px] pr-[137px] "}>
-                                        <div className="">
-                                            <Heading tagName={"h4"} text={"Email me at"} className={"text-[18px] font-medium"} />
-                                            <Heading tagName={"h5"} text={"contact@website.com"} className={""} />
-                                        </div>
-                                        <div className="">
-                                            <Heading tagName={"h4"} text={"Call us"} className={"text-[18px] font-medium"} />
-                                            <Heading tagName={"h5"} text={"0927 6277 28525"} className={"w-[133px]"} />
-                                        </div>
-                                    </Flex>
-                                </div>
-                            </div>
-                            <div className="w-[49%] m-auto">
-                                <Heading tagName={"h3"} text={"Lets Talk!"} className={"text-[48px] font-semibold"} />
-                                <Paragraph className={"w-[359px] pt-3.5 leading-7 text-[#F4F6FC]"}>We are always open to discuss your project, improve your online presence and help with your UX/UI design challenges.</Paragraph>
-                                <Flex className={"gap-x-6 pt-7"}>
-                                    <FaFacebook />
-                                    <FaTwitter />
-                                    <FaInstagram />
-                                    <IoLogoLinkedin />
-                                </Flex>
-                            </div>
-                        </div>
-                    </Container>
-                </section>
-                {/* footer top end */}
-                {/* footer bottom start */}
-                <section className='bg-gray-50'>
-                  <Container>
-                    <div className={"md:flex justify-between py-8 text-black text-center"}>
-                          <div className="">
-                              <Heading tagName={"h4"} text={"Copyright 2025, Finsweet.com"} className={"text-primary "}/>
-                          </div>
-                          <div className="">
-                            <ul className='flex gap-x-8'>
-                        <li className='text-senary hover:text-bg'><Link>Home</Link></li>
-                        <li className='text-senary hover:text-bg'><Link>About us</Link></li>
-                        <li className='text-senary hover:text-bg'><Link>Features</Link></li>
-                        <li className='text-senary hover:text-bg'><Link>Pricing</Link></li>
-                        <li className='text-senary hover:text-bg'><Link>FAQ</Link></li>
-                        <li className='text-senary hover:text-bg'><Link>Blog</Link></li>
-                    </ul>
-                          </div>
-                        </div>
-                  </Container>
-                </section>
-                {/* footer bottom end */}
-            </footer>
-        </>
-    )
-}
+  return (
+    <>
+      <footer>
+        {/* footer top start */}
+        <section className="bg-quaternary pt-[100px] pb-7 text-white font-poppins">
+          <Container>
+            <div className="flex flex-col md:flex-row md:justify-between pb-24 text-center md:text-left relative gap-12 md:gap-0">
+              {/* Left side */}
+              <div className="md:w-[49%] w-full mx-auto relative">
+                <div className="flex flex-col items-center md:items-start">
+                  <Image imgSrc={Logo} />
+                  <Paragraph className="max-w-[359px] pt-[22px] leading-7">
+                    We are always open to discuss your project and improve your online presence.
+                  </Paragraph>
+                </div>
 
-export default Footer
+                
+                <div className="bg-quinary w-full md:max-w-[517px]  h-[155px]  md:h-[95px] md:absolute md:-bottom-7 bottom-0 left-0 mt-8 md:mt-0">
+                  <Flex className="flex-col md:flex-row items-center md:items-start md:justify-between gap-y-4 md:gap-x-12 text-black py-[17px] px-6 md:pl-[37px] md:pr-[60px]">
+                    <div>
+                      <Heading tagName="h4" text="Email me at" className="text-[18px] font-medium" />
+                      <Heading tagName="h5" text="contact@website.com" />
+                    </div>
+                    <div>
+                      <Heading tagName="h4" text="Call us" className="text-[18px] font-medium" />
+                      <Heading tagName="h5" text="0927 6277 28525" className="w-auto" />
+                    </div>
+                  </Flex>
+                </div>
+              </div>
+
+              {/* Right side */}
+              <div className="md:w-[49%] w-full mx-auto mt-10 md:mt-0">
+                <Heading tagName="h3" text="Let’s Talk!" className="text-[36px] md:text-[48px] font-semibold" />
+                <Paragraph className="max-w-[359px] mx-auto md:mx-0 pt-3.5 leading-7 text-[#F4F6FC]">
+                  We are always open to discuss your project, improve your online presence and help with your UX/UI design challenges.
+                </Paragraph>
+                <Flex className="justify-center md:justify-start gap-x-6 pt-7 text-[22px]">
+                  <FaFacebook className="cursor-pointer hover:text-primary duration-300" />
+                  <FaTwitter className="cursor-pointer hover:text-primary duration-300" />
+                  <FaInstagram className="cursor-pointer hover:text-primary duration-300" />
+                  <IoLogoLinkedin className="cursor-pointer hover:text-primary duration-300" />
+                </Flex>
+              </div>
+            </div>
+          </Container>
+        </section>
+        {/* footer top end */}
+
+        {/* footer bottom start */}
+        <section className="bg-gray-50">
+          <Container>
+            <div className="flex flex-col md:flex-row justify-between items-center py-8 text-primary font-medium text-center gap-y-4 md:gap-0">
+              <Heading tagName="h4" text="Copyright 2025, Finsweet.com" className="text-primary" />
+              <ul className="flex flex-wrap justify-center gap-x-6">
+                <li className="text-primary font-medium hover:text-bg duration-300">
+                  <Link to="/">Home</Link>
+                </li>
+                <li className="text-primary font-medium hover:text-bg duration-300">
+                  <Link to="/about">About us</Link>
+                </li>
+                <li className="text-primary font-medium hover:text-bg duration-300">
+                  <Link to="/features">Features</Link>
+                </li>
+                <li className="text-primary font-medium hover:text-bg duration-300">
+                  <Link to="/pricing">Pricing</Link>
+                </li>
+                <li className="text-primary font-medium hover:text-bg duration-300">
+                  <Link to="/faq">FAQ</Link>
+                </li>
+                <li className="text-primary font-medium hover:text-bg duration-300">
+                  <Link to="/blog">Blog</Link>
+                </li>
+              </ul>
+            </div>
+          </Container>
+        </section>
+        {/* footer bottom end */}
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -7,6 +7,7 @@ import Button from '../Button';
 import Image from '../Image';
 import { ImCross } from "react-icons/im";
 import { FaBars } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   const [toggle, setToggle] = useState(false);
@@ -16,7 +17,7 @@ const Header = () => {
       <header className="relative z-50">
         <nav className="bg-quaternary text-white py-4 font-poppins">
           <Container>
-            <Flex className="justify-between items-center">
+            <Flex className="justify-between items-center px-5 md:px-0">
               {/* Logo */}
               <div>
                 <Image imgSrc={Logo} />
@@ -24,28 +25,127 @@ const Header = () => {
 
               {/* Desktop Menu */}
               <ul className="hidden md:flex gap-x-8">
-                <li className="text-senary hover:text-white transition"><Link to="/">Home</Link></li>
-                <li className="text-senary hover:text-white transition"><Link to="/about">About us</Link></li>
-                <li className="text-senary hover:text-white transition"><Link to="/features">Features</Link></li>
-                <li className="text-senary hover:text-white transition"><Link to="/pricing">Pricing</Link></li>
-                <li className="text-senary hover:text-white transition"><Link to="/read">Case</Link></li>
-                <li className="text-senary hover:text-white transition"><Link to="/blog">Blog</Link></li>
-                <li className="text-senary hover:text-white transition"><Link to="/work">Portfolio</Link></li>
-                <li className="text-senary hover:text-white transition"><Link to="/readblog">Read Blog</Link></li>
-                <li className="text-senary hover:text-white transition"><Link to="/policy">Policy</Link></li>
-                <li className="text-senary hover:text-white transition"><Link to="/contact">Contact</Link></li>
+                <li className="text-senary hover:text-white transition">
+                  <NavLink
+                    to="/"
+                    className={({ isActive }) =>
+                      isActive ? "text-white font-semibold" : "text-[#BBBBCB]"
+                    }
+                  >
+                    Home
+                  </NavLink>
+                </li>
+
+                <li className="text-senary hover:text-white transition">
+                  <NavLink
+                    to="/about"
+                    className={({ isActive }) =>
+                      isActive ? "text-white font-semibold" : "text-[#BBBBCB]"
+                    }
+                  >
+                    About us
+                  </NavLink>
+                </li>
+
+                <li className="text-senary hover:text-white transition">
+                  <NavLink
+                    to="/features"
+                    className={({ isActive }) =>
+                      isActive ? "text-white font-semibold" : "text-[#BBBBCB]"
+                    }
+                  >
+                    Features
+                  </NavLink>
+                </li>
+
+                <li className="text-senary hover:text-white transition">
+                  <NavLink
+                    to="/pricing"
+                    className={({ isActive }) =>
+                      isActive ? "text-white font-semibold" : "text-[#BBBBCB]"
+                    }
+                  >
+                    Pricing
+                  </NavLink>
+                </li>
+
+                <li className="text-senary hover:text-white transition">
+                  <NavLink
+                    to="/read"
+                    className={({ isActive }) =>
+                      isActive ? "text-white font-semibold" : "text-[#BBBBCB]"
+                    }
+                  >
+                    Case
+                  </NavLink>
+                </li>
+
+                <li className="text-senary hover:text-white transition">
+                  <NavLink
+                    to="/blog"
+                    className={({ isActive }) =>
+                      isActive ? "text-white font-semibold" : "text-[#BBBBCB]"
+                    }
+                  >
+                    Blog
+                  </NavLink>
+                </li>
+
+                <li className="text-senary hover:text-white transition">
+                  <NavLink
+                    to="/work"
+                    className={({ isActive }) =>
+                      isActive ? "text-white font-semibold" : "text-[#BBBBCB]"
+                    }
+                  >
+                    Portfolio
+                  </NavLink>
+                </li>
+
+                <li className="text-senary hover:text-white transition">
+                  <NavLink
+                    to="/readblog"
+                    className={({ isActive }) =>
+                      isActive ? "text-white font-semibold" : "text-[#BBBBCB]"
+                    }
+                  >
+                    Read Blog
+                  </NavLink>
+                </li>
+
+                <li className="text-senary hover:text-white transition">
+                  <NavLink
+                    to="/policy"
+                    className={({ isActive }) =>
+                      isActive ? "text-white font-semibold" : "text-[#BBBBCB]"
+                    }
+                  >
+                    Policy
+                  </NavLink>
+                </li>
+
+                <li className="text-senary hover:text-white transition">
+                  <NavLink
+                    to="/contact"
+                    className={({ isActive }) =>
+                      isActive ? "text-white font-semibold" : "text-[#BBBBCB]"
+                    }
+                  >
+                    Contact
+                  </NavLink>
+                </li>
               </ul>
 
               {/* Desktop Button */}
               <div className="hidden md:flex">
                 <Button
-                  className="py-3 px-10 border border-[rgba(244,246,252,0.2)] hover:bg-white hover:text-quaternary transition"
+                  className="py-3 px-10 border border-[rgba(244,246,252,0.2)] hover:bg-white hover:text-quaternary transition text-white"
                   text="Contact us"
                 />
               </div>
 
               {/* Mobile Menu Toggle */}
-              
+
               <button
                 className="md:hidden"
                 onClick={() => setToggle(!toggle)}
@@ -53,24 +153,83 @@ const Header = () => {
               >
                 {toggle ? <ImCross size={26} /> : <FaBars size={26} />}
               </button>
-              
+
             </Flex>
           </Container>
         </nav>
 
         {/* Mobile Dropdown Menu */}
         {toggle && (
-          <div className="absolute top-full left-0 w-full bg-quaternary text-center py-6 md:hidden transition-all duration-300">
+          <div className="absolute top-full left-0 w-full bg-quaternary text-center py-6 md:hidden transition-all duration-700 delay-700">
             <ul className="space-y-4">
-              <li className="text-senary hover:text-white"><Link to="/">Home</Link></li>
-              <li className="text-senary hover:text-white"><Link to="/about">About us</Link></li>
-              <li className="text-senary hover:text-white"><Link to="/features">Features</Link></li>
-              <li className="text-senary hover:text-white"><Link to="/pricing">Pricing</Link></li>
-              <li className="text-senary hover:text-white"><Link to="/faq">FAQ</Link></li>
-              <li className="text-senary hover:text-white"><Link to="/blog">Blog</Link></li>
+                <li className="text-senary hover:text-white transition">
+                  <NavLink
+                    to="/"
+                    className={({ isActive }) =>
+                      isActive ? "text-white font-semibold" : "text-[#BBBBCB]"
+                    }
+                  >
+                    Home
+                  </NavLink>
+                </li>
+
+                <li className="text-senary hover:text-white transition">
+                  <NavLink
+                    to="/about"
+                    className={({ isActive }) =>
+                      isActive ? "text-white font-semibold" : "text-[#BBBBCB]"
+                    }
+                  >
+                    About us
+                  </NavLink>
+                </li>
+
+                <li className="text-senary hover:text-white transition">
+                  <NavLink
+                    to="/features"
+                    className={({ isActive }) =>
+                      isActive ? "text-white font-semibold" : "text-[#BBBBCB]"
+                    }
+                  >
+                    Features
+                  </NavLink>
+                </li>
+
+                <li className="text-senary hover:text-white transition">
+                  <NavLink
+                    to="/pricing"
+                    className={({ isActive }) =>
+                      isActive ? "text-white font-semibold" : "text-[#BBBBCB]"
+                    }
+                  >
+                    Pricing
+                  </NavLink>
+                </li>
+
+                <li className="text-senary hover:text-white transition">
+                  <NavLink
+                    to="/read"
+                    className={({ isActive }) =>
+                      isActive ? "text-white font-semibold" : "text-[#BBBBCB]"
+                    }
+                  >
+                    Case
+                  </NavLink>
+                </li>
+
+                <li className="text-senary hover:text-white transition">
+                  <NavLink
+                    to="/blog"
+                    className={({ isActive }) =>
+                      isActive ? "text-white font-semibold" : "text-[#BBBBCB]"
+                    }
+                  >
+                    Blog
+                  </NavLink>
+                </li>
               <li>
                 <Button
-                  className="py-3 px-10 border border-[rgba(244,246,252,0.2)] hover:bg-white hover:text-quaternary transition"
+                  className="py-3 px-10 border border-[rgba(244,246,252,0.2)] hover:bg-white hover:text-quaternary transition text-white"
                   text="Contact us"
                 />
               </li>
