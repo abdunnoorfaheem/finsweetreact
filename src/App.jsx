ff845c2afd28d37068207fd2fdbf36db56786858
eaheabinayub <eahea.sbmc403@gmail.com>
2025-10-27 22:19:42 +0600

    responsive done

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ function App() {
       <Routes>
         <Route path="/" element={<RootLayout />}>
           <Route index element={<Home />} />
-          <Route path="/pricing" element={<Pricing />} />
         </Route>
       </Routes>
     </>
