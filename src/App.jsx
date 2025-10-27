bdf4f748092a34d8194f3206c4e3335c4d6f4bda
Mansur <hmmansur050@gmail.com>
2025-10-28 01:13:13 +0600

    about part responsive update

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     <Routes>
       <Route path='/' element={<RootLayout/>}>
       <Route index element={<Home/>}/>
-      <Route path='/about' element={<About/>}/>
+      
       </Route>
     </Routes>
     </>
